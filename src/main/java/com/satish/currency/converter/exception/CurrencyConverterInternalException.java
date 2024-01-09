1d0c5f0c1061b4aa860ce143dc15de1bb6468936
Kamavaram Satish Kumar <satishkamavaram@gmail.com>
2024-01-09 11:37:57 +0530

    formatting

--- src/main/java/com/satish/currency/converter/exception/CurrencyConverterInternalException.java
+++ src/main/java/com/satish/currency/converter/exception/CurrencyConverterInternalException.java
@@ -5,7 +5,7 @@ public class CurrencyConverterInternalException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public CurrencyConverterInternalException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
+        	super(message, cause);
+        }
 	
 }
