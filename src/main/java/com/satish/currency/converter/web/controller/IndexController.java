747906233813122522fb89ac876223b6ce2fb146
Kamavaram Satish Kumar <satishkamavaram@gmail.com>
2024-01-09 11:32:40 +0530

    Update IndexController.java
    test

--- src/main/java/com/satish/currency/converter/web/controller/IndexController.java
+++ src/main/java/com/satish/currency/converter/web/controller/IndexController.java
@@ -10,17 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    /**
-     * Index endpoint to show the index page
-     *
-     * @param model Spring's view model
-     * @return view name
-     */
-    @GetMapping({"/", "/index"})
-    public String index(Model model) {
-        model.addAttribute("title", "Check24 Task");
-        model.addAttribute("welcome", "Welcome to Check24");
-        model.addAttribute("applicationTitle", "Check24 Currency Converter");
-        return "index";
-    }
+    
 }
