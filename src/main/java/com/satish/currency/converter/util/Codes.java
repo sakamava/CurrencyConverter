ad41350bbe1b90f7fe3e383bc995e9c67784373a
Kamavaram Satish Kumar <satishkamavaram@gmail.com>
2024-01-09 11:40:22 +0530

    formatting

--- src/main/java/com/satish/currency/converter/util/Codes.java
+++ src/main/java/com/satish/currency/converter/util/Codes.java
@@ -2,11 +2,11 @@ package com.satish.currency.converter.util;
 
 public enum Codes {
 //TODO read description from application properties, this will allow to change description without source code changes.
-	SUCCESS("S0000", "Success"),
+    SUCCESS("S0000", "Success"),
     BASE_CODE_NOT_FOUND("E0001", "Base Code not found"),
     NO_DATA_ERROR("E3002", "No data found"),
     CONSTRAINT_VIOLATION_ERROR("E3003", "Validation Error"),
-	INTERNAL_SERVER_ERROR("E3020", "Internal Server Error");
+    INTERNAL_SERVER_ERROR("E3020", "Internal Server Error");
 	
 
     private final String code;
